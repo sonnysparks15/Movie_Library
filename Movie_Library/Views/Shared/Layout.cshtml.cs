34f1491db434b6b0d260ab47d1d3d6983b389abc
Sonny Sparks <sonny.sparks15@gmail.com>
2022-05-10 11:03:14 -0500

    Finished Product CSS and SQL Movie Library

---
+++ Movie_Library/Views/Shared/Layout.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Movie_Library.Views.Shared;
+
+public class Layout : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Movie_Library/Views/Shared/_LLayout.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Movie_Library.Views.Shared;
+
+public class _LLayout : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
